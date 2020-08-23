181385e2fd5e95593e43b15b66e9503e2394a7a2
张红豆 <zhanghongdou@enn.cn>
2020-08-23 13:02:24 +0800

    提交第一个接口多态的例子

---
+++ src/day11/DemoInterface.java
@@ -0,0 +1,22 @@
+package day11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoInterface {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        List<String> result = addNames(list);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
+
+    public static List<String> addNames(List<String> list) {
+        list.add("迪丽热巴");
+        list.add("古力娜扎");
+        list.add("马尔扎哈");
+        list.add("欧阳娜娜");
+        return list;
+    }
+}
